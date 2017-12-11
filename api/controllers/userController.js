6883625e7f1f077f6303c6b81f585ec117e964ea
Milton Espinoza <mespinoza@rally-now.localdomain>
2017-12-11 18:45:21 +0000

    adding social access token to user controller

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -91,7 +91,7 @@ exports.create = function(req, res, callback){
 	 console.log(req.params);
 	console.log('--------------req----------------');
 	console.log(req);
-         new con.Request("insert into [users] (name, username, password, email, birthday, lat, lon, created_at, updated_at, verification_code, photo_url, azure_id) values( @name, @username, @password, @email, @birthday, @lat, @lon, @created_at, @updated_at, @verification_code, @photo_url, @azure_id); select * from [users] where id = scope_identity();") 
+         new con.Request("insert into [users] (name, username, password, email, birthday, lat, lon, created_at, updated_at, verification_code, photo_url, azure_id, social_access_token) values( @name, @username, @password, @email, @birthday, @lat, @lon, @created_at, @updated_at, @verification_code, @photo_url, @azure_id, @social_access_token); select * from [users] where id = scope_identity();") 
             .addParam("name", TYPES.VarChar, req.body.name)  
             .addParam("username", TYPES.VarChar, req.body.username) 
             .addParam("password", TYPES.VarChar, req.body.password) 
@@ -100,8 +100,9 @@ exports.create = function(req, res, callback){
             .addParam("lat", TYPES.VarChar, req.body.lat)  
             .addParam("lon", TYPES.VarChar, req.body.lon) 
             .addParam("verification_code", TYPES.VarChar, code) 
-	    .addParam("photo_url", TYPES.VarChar, req.body.photo_url)
-	    .addParam("azure_id", TYPES.VarChar, req.body.azure_id)
+						.addParam("photo_url", TYPES.VarChar, req.body.photo_url)
+						.addParam("azure_id", TYPES.VarChar, req.body.azure_id)
+						.addParam("social_access_token", TYPES.VarChar, req.body.social_access_token)
 
             .addParam("created_at", TYPES.VarChar, moment().format('YYYY-MM-DD'))
             .addParam("updated_at", TYPES.VarChar, moment().format('YYYY-MM-DD'))  
@@ -132,7 +133,7 @@ exports.update = function(req, res, callback){
     var con = new msSqlConnecter.msSqlConnecter(dbConfig.config); 
     con.connect().then(function () { 
         console.log("moment request at "+moment().format('YYYY-MM-DD'));
-        new con.Request("update [users] set name=@name, username=@username, password=@password, email=@email, birthday=@birthday, lat=@lat, lon=@lon, photo_url=@photo_url, updated_at=@updated_at  where id = @id; select * from [users] where id = @id;") 
+        new con.Request("update [users] set name=@name, username=@username, password=@password, email=@email, birthday=@birthday, lat=@lat, lon=@lon, photo_url=@photo_url, updated_at=@updated_at, social_access_token=@social_access_token  where id = @id; select * from [users] where id = @id;") 
         .addParam("id", TYPES.Int, Number(req.params.userId)  )
         .addParam("name", TYPES.VarChar, req.body.name) 
         .addParam("username", TYPES.VarChar, req.body.username) 
@@ -141,7 +142,8 @@ exports.update = function(req, res, callback){
         .addParam("birthday", TYPES.VarChar, req.body.birthday)
         .addParam("lat", TYPES.VarChar, req.body.lat)
         .addParam("lon", TYPES.VarChar, req.body.lon)
-	.addParam("photo_url", TYPES.VarChar, req.body.photo_url)
+				.addParam("photo_url", TYPES.VarChar, req.body.photo_url)
+				.addParam("social_access_token", TYPES.VarChar, req.body.social_access_token)
         .addParam("updated_at", TYPES.Date, moment().format('YYYY-MM-DD')) 
         .onComplate(function (count, data) {                  
             res.send(data);
