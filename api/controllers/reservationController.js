55702d7b9375e79dcf7a08c8589c3649639f0fff
DESKTOP-IA53K1I\Erick Nunez <osmany@senorcoders.com>
2018-01-08 11:58:59 -0600

    Agregue mas datos al endpoint de las reservations not passed

--- api/controllers/reservationController.js
+++ api/controllers/reservationController.js
@@ -201,7 +201,46 @@ exports.delete = function(req, res){
 exports.getAllNotPassed = (req, res)=>{
     let db = require("./../bd")
     let query = String.raw`
-        select * from reservations where CAST('${ req.params.dateNow }' AS DATE) <= transaction_end_date
+    select 
+    rs.id as reservationID,
+    rs.transaction_date as transactionDate,
+    rs.transaction_start_date as transactionStartDate,
+    rs.transaction_end_date as transactionEndtTime,
+    rs.transaction_start_time as transactionStartTime,
+    rs.transaction_end_time as transactionEndTime,
+    rs.price as Price,
+    op.id as operatorID,
+    op.operator_name as operatorName,
+    op.operator_type as operatorType,
+    op.time_zone as operatorTimeZona,
+    op.currency as operatorCurrency,
+    op.business_type as operatorBusinessType,
+    pds.id as productID,
+    pds.name as productName,
+    pr.id as priceID,
+    pr.price as productPrice,
+    pr.price_plan as pricePlan,
+    pr.available_date as priceAvailableDate,
+    pr.available_time as priceAvailableTime,
+    pr.end_date as priceEndDate,
+    pr.end_time as priceEndTime,
+    loc.id as locationID,
+    loc.lat as locationLatitud,
+    loc.lot as locationLongitud,
+    loc.name as locationName,
+    loc.currency as locationCurrency,
+    loc.address as locationAddress,
+    loc.hours_operation as locationHoursOperation,
+    loc.website as locationWebSite,
+    loc.country as locationCountry,
+    loc.contact_name as locationContactName,
+    loc.contact_email as locationContactEmail
+    from products pds
+    INNER JOIN reservations rs  on pds.id = rs.product_id 
+    INNER JOIN operator op on pds.operator_id = op.id
+    inner JOIN pricing pr on pr.product_id = pds.id
+    inner join locations loc on loc.id = pds.location_id
+    where CAST('${ req.params.dateNow}' AS DATE) <= rs.transaction_end_date
     `
     db.query(query)
     .then(data=>{ res.send(data[0]) })
