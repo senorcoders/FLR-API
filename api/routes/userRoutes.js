8af287a3f43f06273550f8540ac08d2b807e1768
Osmayteam <osmany@senorcoders.com>
2018-01-09 10:55:27 -0600

    Agregue el endpoint para obtener todas las reservations de un usuario

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -34,4 +34,8 @@ module.exports = function(app) {
   app.route(endpoint+ "/:id/stars-comments")
   .get(require("./../controllers/starsComments").getAllXUser)
 
+  //get all the comments y stars
+  app.route(endpoint+ "/:id/reservations")
+  .get(require("./../controllers/reservationController").getXUserAll)
+
 };
