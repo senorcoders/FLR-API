6f102151cd49c628bc215d67e7e27fe86de9f9d9
Milton Espinoza <milton@senorcoders.com>
2018-02-07 11:44:12 -0600

    removing innesesary files

--- api/models/payment.js
+++ api/models/payment.js
@@ -6,28 +6,28 @@ const db = require("./../bd")
 module.exports = (sequelize, DataTypes)=>{
     let payment = sequelize.define('payment', {
         id : { type: Sequelize.INTEGER, primaryKey : true, autoIncrement: true },
-        reservation_id : { type : Sequelize.INTEGER, references : { model : "reservations", key : "id", allowNull : false } },        
+        reservation_id : { type : Sequelize.INTEGER, references : { model : "reservations", key : "id", allowNull : true } },        
         amount : { type : Sequelize.STRING, allowNull : false },
         resptext : { type : Sequelize.STRING, allowNull : false },
-        commandcard : { type : Sequelize.STRING, allowNull : false },
-        cvvresp : { type : Sequelize.STRING, allowNull : false },
-        batchid : { type : Sequelize.STRING, allowNull : false },
-        avsresp : { type : Sequelize.STRING, allowNull : false },
+        commandcard : { type : Sequelize.STRING, allowNull : true },
+        cvvresp : { type : Sequelize.STRING, allowNull : true },
+        batchid : { type : Sequelize.STRING, allowNull : true },
+        avsresp : { type : Sequelize.STRING, allowNull : true },
         respcode : { type : Sequelize.STRING, allowNull : false },
-				merchid : { type : Sequelize.STRING, allowNull : false },
-				token : { type : Sequelize.STRING, allowNull : false },
-				authcode : { type : Sequelize.STRING, allowNull : false },
-				respproc : { type : Sequelize.STRING, allowNull : false },
-				retref : { type : Sequelize.STRING, allowNull : false },
-				retstat : { type : Sequelize.STRING, allowNull : false },
-				account : { type : Sequelize.STRING, allowNull : false },
-				currency : { type : Sequelize.STRING, allowNull : false }
+		merchid : { type : Sequelize.STRING, allowNull : false },
+		token : { type : Sequelize.STRING, allowNull : true },
+		authcode : { type : Sequelize.STRING, allowNull : true },
+		respproc : { type : Sequelize.STRING, allowNull : false },
+		retref : { type : Sequelize.STRING, allowNull : false },
+		retstat : { type : Sequelize.STRING, allowNull : true },
+		account : { type : Sequelize.STRING, allowNull : true },
+		currency : { type : Sequelize.STRING, allowNull : false }
     }, {
         freezeTableName: true,
-        tableName: 'comments_operators'
+        tableName: 'payment'
       });
     
-      //sequelize.sync()
+      sequelize.sync()
 
     return payment
 }
