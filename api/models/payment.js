b4ea0ffdc20f78c153803fa85f897215d6e1beeb
Milton Espinoza <mespinoza@rally-now.localdomain>
2018-02-07 17:08:25 +0000

    fix table name

--- api/models/payment.js
+++ api/models/payment.js
@@ -6,10 +6,10 @@ const db = require("./../bd")
 module.exports = (sequelize, DataTypes)=>{
     let payment = sequelize.define('payment', {
         id : { type: Sequelize.INTEGER, primaryKey : true, autoIncrement: true },
-        reservation_id : { type : Sequelize.INTEGER, references : { model : "reservations", key : "id", allowNull : false } },        
+        //reservation_id : { type : Sequelize.INTEGER, references : { model : "reservations", key : "id", allowNull : false } },        
         amount : { type : Sequelize.STRING, allowNull : false },
         resptext : { type : Sequelize.STRING, allowNull : false },
-        commandcard : { type : Sequelize.STRING, allowNull : false },
+        commandcard : { type : Sequelize.STRING, allowNull : true },
         cvvresp : { type : Sequelize.STRING, allowNull : false },
         batchid : { type : Sequelize.STRING, allowNull : false },
         avsresp : { type : Sequelize.STRING, allowNull : false },
@@ -19,15 +19,15 @@ module.exports = (sequelize, DataTypes)=>{
 				authcode : { type : Sequelize.STRING, allowNull : false },
 				respproc : { type : Sequelize.STRING, allowNull : false },
 				retref : { type : Sequelize.STRING, allowNull : false },
-				retstat : { type : Sequelize.STRING, allowNull : false },
+				retstat : { type : Sequelize.STRING, allowNull : true },
 				account : { type : Sequelize.STRING, allowNull : false },
 				currency : { type : Sequelize.STRING, allowNull : false }
     }, {
         freezeTableName: true,
-        tableName: 'comments_operators'
+        tableName: 'payment'
       });
     
-      //sequelize.sync()
+//      sequelize.sync()
 
     return payment
 }
