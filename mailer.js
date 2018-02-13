13f2f238f6a51352c73c36eec559294822384ba1
Osmanyteam <jos.ojiron@gmail.com>
2018-02-13 10:23:25 -0600

    Add for send notification email of inquiry to user

--- mailer.js
+++ mailer.js
@@ -220,9 +220,97 @@ function sendNotificationAdmin(user, reservation, template){
             
         });
 }
-//#endregion
 
 //usando funciones compuestas
 exports
 .
-sendNotifications = (user, reservation, product)=> getTemplatesUser(user, reservation, product)(getTemplatesAdmin)
+sendNotifications = (user, reservation, product)=> getTemplatesUser(user, reservation, product)
+(getTemplatesAdmin)
+
+//#endregion
+
+//#region for send email notification request /inquery
+function getTemplatesInquiry(user, product){
+    const body = String.raw`
+    <div class="">
+	<!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 10px; padding-left: 10px; padding-top: 10px; padding-bottom: 10px;"><![endif]-->
+	<div style="color:#4A443E;line-height:120%;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif; padding-right: 10px; padding-left: 10px; padding-top: 10px; padding-bottom: 10px;">	
+        <div style="font-size:12px;line-height:14px;color:#4A443E;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;text-align:left;"><p style="margin: 0;font-size: 12px;line-height: 14px;text-align: left"><span style="font-size: 20px; line-height: 24px;">
+        Hello!, You have made a new request for a ${product.name}  the service with&#160; ${product.service_type} </span></p><p style="margin: 0;font-size: 12px;line-height: 14px"><span style="font-size: 20px; line-height: 24px;"><br data-mce-bogus="1"></span></p></div>	
+	</div>
+	<!--[if mso]></td></tr></table><![endif]-->
+    </div>
+    `
+
+    this.callback = function(){
+        console.log("callback");
+    }
+
+    fs.readFile("./template_emails/inquiry.1.html", "utf8", function(err, data){
+        fs.readFile("./template_emails/inquiry.2.html", "utf8", function(err, data2){
+            callback(user, (data+ body+ data2))
+        })
+    })
+
+    return function(callback){
+        this.callback = callback;
+    }
+}
+
+function sendNotificationUserInqury(user, template){
+    'use strict'
+    console.log('sending email to '+ user.email);
+    const nodemailer = require('nodemailer');
+
+    
+    
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    nodemailer.createTestAccount((err, account) => {
+        
+            // create reusable transporter object using the default SMTP transport
+            let transporter = nodemailer.createTransport({
+                host: 'smtp.zoho.com',
+                port: 465,
+                secure: true, // true for 465, false for other ports
+                auth: {
+                    user: 'milton@senorcoders.com', // generated ethereal user
+                    pass: 'KykPjRehFtsQ'  // generated ethereal password
+                }
+            });
+            
+            let mailOptionsAdmin = {
+                from: '"Senorcoders" <milton@senorcoders.com>', // sender address
+                to: user.email, // list of receivers
+                subject: 'New Request', // Subject line
+                text: '', // plain text body
+                html: template, // html body
+                attachments: [{
+                    filename: 'image.png',
+                    path: './template_emails/images/image.png',
+                    cid: 'unique@kreata.ee' //same cid value as in the html img src
+                }]
+            };
+        
+            // send mail with defined transport object
+            transporter.sendMail(mailOptionsAdmin, (error, info) => {
+                if (error) {
+                    return console.log(error);
+                }
+                console.log('Message sent: %s', info.messageId);
+                // Preview only available when sending through an Ethereal account
+                console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        
+                // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
+                // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+            });
+            
+        });
+}
+
+exports
+.
+sendNoticationInquiry = (user, product)=> getTemplatesInquiry(user, product)
+(sendNotificationUserInqury)
+
+//#endregion
