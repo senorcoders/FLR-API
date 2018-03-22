779f5b838832588b3f45fe9d203231ca39e53688
Osmany Jirón <osmany@senorcoders.com>
2018-03-22 14:22:31 -0600

    changes

--- api/controllers/starsComments.js
+++ api/controllers/starsComments.js
@@ -1,10 +1,40 @@
 'use strict'
 const bd = require("../bd")
 
+function averageStar(reviews){
+    let average = {};
+    for(let review of reviews){
+        let stars = review.numStars.toString();
+        if( average.hasOwnProperty(stars) ){
+            average[stars] = parseInt(average[stars]) + 1;
+        }else{
+            average[stars] = 1;
+        }
+    }
+    let total = 0;
+    
+    for(var av of Object.keys(average)){
+        total += parseInt(av);
+    }
+    average.total = total;
+    
+    return average;
+}
+
 module.exports = {
-    getAllXOperator : (req, res, next)=>{
+    getAllXOperatorAverage : (req, res, next)=>{
         let query = String.raw`
-        select id, user_id as userID, operator_id as operatorID, start as numStars from star_operators where operator_id = ${ req.params.id }
+        SELECT *
+        FROM (
+            select star_operators.id, star_operators.operator_id, star_operators.user_id as userID, star_operators.operator_id as operatorID,
+            start as numStars, products.name_image, reservations.ID as bookingID, reservations.transaction_date,
+            operator.operator_name, ROW_NUMBER() OVER (PARTITION BY star_operators.id ORDER BY star_operators.id ASC) as rn
+            from star_operators inner JOIN products on products.operator_id = star_operators.operator_id
+            inner join reservations on reservations.product_id = products.id
+            INNER JOIN operator on operator.id = products.operator_id
+            where star_operators.operator_id = ${ req.params.id } ) 
+        a
+        WHERE rn = 1
         `
 
         bd.query(query)
@@ -34,7 +64,8 @@ module.exports = {
                         if( i < data[0].length ){
                             gets(i)
                         }else{
-                            res.send(data[0])
+                            let average = averageStar(data[0])
+                            res.send({reviews : data[0], average })
                         }
                         
                     })
@@ -56,7 +87,7 @@ module.exports = {
             res.send(err)
         })
     },
-    getAllXUser : (req, res, next)=>{
+    getAllXUserStarComments : (req, res, next)=>{
         let query = String.raw`
         select id, user_id as userID, operator_id as operatorID, start as numStars from star_operators where user_id = ${ req.params.id }
         `
@@ -88,7 +119,8 @@ module.exports = {
                         if( i < data[0].length ){
                             gets(i)
                         }else{
-                            res.send(data[0])
+                            let average = averageStar(data[0]);
+                            res.send({ reviews : data[0], average })
                         }
                         
                     })
@@ -108,9 +140,73 @@ module.exports = {
             res.send(err)
         })
     },
+    getAllXUserStarCommentsMoreData : (req, res, next)=>{
+        let query = String.raw`
+        SELECT *
+        FROM (
+            select star_operators.id, star_operators.operator_id, star_operators.user_id as userID, star_operators.operator_id as operatorID,
+            start as numStars, products.name_image, reservations.ID as bookingID, reservations.transaction_date,
+            operator.operator_name, ROW_NUMBER() OVER (PARTITION BY star_operators.id ORDER BY star_operators.id ASC) as rn
+            from star_operators inner JOIN products on products.operator_id = star_operators.operator_id
+            inner join reservations on reservations.product_id = products.id
+            INNER JOIN operator on operator.id = products.operator_id
+            where star_operators.user_id = ${ req.params.id } ) 
+        a
+        WHERE rn = 1
+        `
+
+        bd.query(query)
+        .then((data)=>{
+            res.send(data[0])
+            /*if( data[0].length > 0 )
+            {
+                function gets(i){
+                let query = String.raw`
+                select comment.id as commentID, 
+                comment.content as commentContent, 
+                comment.createdAt as dateCreate,
+                operator.id as operatorID,
+                operator.operator_name as operatorName,
+                operator.operator_type as operatorType,
+                operator.currency as operatorCurrency,
+                operator.business_type as operatorBussinesType
+                from comments_operators comment INNER JOIN operator on comment.operator_id = operator.id where user_id = ${ req.params.id } and operator.id = ${ data[0][i].operatorID } order by convert(datetime, comment.createdAt) DESC                
+                `
+                bd.query(query)
+                    .then(function(d){
+                        if( d[0] !== undefined )
+                            data[0][i].comments = d[0]
+
+                        console.log(data[0].length)
+                        i++
+                        if( i < data[0].length ){
+                            gets(i)
+                        }else{
+                            
+                            res.send(data[0])
+                        }
+                        
+                    })
+                    .catch((err)=>{
+                        console.error(err.message)
+                        res.send([])
+                    })
+                }
+
+                gets(0)
+            }else{
+                res.send([])
+            }*/
+            
+        })
+        .catch((err)=>{
+            console.error(err.message)
+            res.send(err)
+        })
+    },
     getAllXUserNotContains : (req, res, next)=>{
         let query = String.raw`
-        select DISTINCT operator.* from products INNER JOIN reservations on reservations.product_id = products.id
+        select DISTINCT operator.*, products.name_image, reservations.ID, reservations.transaction_date from products INNER JOIN reservations on reservations.product_id = products.id
         INNER JOIN operator on operator.id = products.operator_id
         where reservations.user_id = ${req.params.userId}
         and operator.id not in(
